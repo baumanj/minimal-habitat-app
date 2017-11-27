ee7bdcb91cda2eff1970d8d156a6bbcdc97e882f
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2017-11-27 15:41:50 -0800

    first commit

---
+++ habitat/plan.sh
@@ -0,0 +1,26 @@
+pkg_origin=core
+pkg_name=minimial-habitat-app
+pkg_version=0.1.0
+pkg_source=nosuchfile.tar.gz
+pkg_deps=(core/minimial-habitat-app)
+pkg_expose=()
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
